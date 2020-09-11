f3db79c0d36f405573b930ebbc67a20122f212da
AustinKavelman <kavea9410@wrdsb.ca>
2020-09-11 13:33:17 +0000

    Finished the Hello world problem.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
- *
- * @author 
+ * This program prints hello world to the screen
+ * @author Austin Kavelman
  */
 public class Main {
 
@@ -9,7 +9,8 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Prints "Hello World" to the screen
+    System.out.println("Hello World!");
     
   }
 }

716327f865ac04f76754b14dffad1c56f35f9d50
Adam Lamont <adam_lamont@wrdsb.ca>
2020-09-11 09:04:01 -0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author 
+ */
+public class Main {
+
+  /**
+   * The method that is executed when you hit the run button.
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+    
+  }
+}
